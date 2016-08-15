71e6eb438b8b11a0c30753b6faf24df78a07c7bc
nicolwon <nicwongsoo@gmail.com>
2016-08-14 21:13:59 -0400

    Add files via upload

---
+++ flitt/flitt/FirstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.swift
+//  flitt
+//
+//  Created by Nicole Wongsoo on 2016-08-14.
+//  Copyright © 2016 Nicole Wongsoo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    
+    @IBAction func testButton(sender: UIButton) {
+    }
+    
+}
+
---
+++ flitt/flitt/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  flitt
+//
+//  Created by Nicole Wongsoo on 2016-08-14.
+//  Copyright © 2016 Nicole Wongsoo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
